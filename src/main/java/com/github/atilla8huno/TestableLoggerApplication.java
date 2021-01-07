ef66e6d30505d261e2b09e06b102891977ca3a80
Atilla Barros <atilla.barros@gamesys.co.uk>
2021-01-07 15:21:57 +0000

    Initial java/spring setup

---
+++ src/main/java/com/github/atilla8huno/TestableLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.github.atilla8huno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestableLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestableLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/atilla8huno/TestableLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.atilla8huno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestableLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
